41377a79749e6bc1902da5bd044648f3effd4c70
CMU\attaphon_wong <boatbright00@gmail.com>
2017-09-14 22:08:27 +0700

    Begin Website With HerokuApp

---
+++ DesktopApp Terminal NodeMCU/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private object keyboardProc;
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process[] oskProcessArray = Process.GetProcessesByName("TabTip");
+            foreach (Process onscreenProcess in oskProcessArray)
+            {
+                onscreenProcess.Kill();
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            string progFiles = @"C:\Program Files\Common Files\Microsoft Shared\ink";
+            string keyboardPath = Path.Combine(progFiles, "TabTip.exe");
+            this.keyboardProc = Process.Start(keyboardPath);
+        }
+    }
+}
